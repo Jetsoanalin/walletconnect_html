30515fd8fcb4577f8515af6d4af42b99cd675b4e
Jetso Analin <jetyjetso@gmail.com>
2021-12-29 04:28:56 +0530

    New codebase changed

--- example.js
+++ example.js
@@ -72,8 +72,8 @@ async function fetchAccountData() {
   // Get connected chain id from Ethereum node
   const chainId = await web3.eth.getChainId();
   // Load chain information over an HTTP API
-  const chainData = await EvmChains.getChain(chainId);
-  document.querySelector("#network-name").textContent = chainData.name;
+  // const chainData = await EvmChains.getChain(chainId);
+  // document.querySelector("#network-name").textContent = chainData.name;
 
   // Get list of accounts of the connected wallet
   const accounts = await web3.eth.getAccounts();
