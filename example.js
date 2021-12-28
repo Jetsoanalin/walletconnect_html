1e08666c60a0014382f47f815db0f524e34840aa
Jetso Analin <jetyjetso@gmail.com>
2021-12-29 03:15:55 +0530

    New codebase changed

--- example.js
+++ example.js
@@ -7,8 +7,8 @@
  // Unpkg imports
 const Web3Modal = window.Web3Modal.default;
 const WalletConnectProvider = window.WalletConnectProvider.default;
+const EvmChains = window.EvmChains;
 const Fortmatic = window.Fortmatic;
-const evmChains = window.evmChains;
 
 // Web3modal instance
 let web3Modal
@@ -29,17 +29,6 @@ function init() {
   console.log("Initializing example");
   console.log("WalletConnectProvider is", WalletConnectProvider);
   console.log("Fortmatic is", Fortmatic);
-  console.log("window.web3 is", window.web3, "window.ethereum is", window.ethereum);
-
-  // Check that the web page is run in a secure context,
-  // as otherwise MetaMask won't be available
-  if(location.protocol !== 'https:') {
-    // https://ethereum.stackexchange.com/a/62217/620
-    const alert = document.querySelector("#alert-error-https");
-    alert.style.display = "block";
-    document.querySelector("#btn-connect").setAttribute("disabled", "disabled")
-    return;
-  }
 
   // Tell Web3modal what providers we have available.
   // Built-in web browser provider (only one can exist as a time)
@@ -65,10 +54,8 @@ function init() {
   web3Modal = new Web3Modal({
     cacheProvider: false, // optional
     providerOptions, // required
-    disableInjectedProvider: false, // optional. For MetaMask / Brave / Opera.
   });
 
-  console.log("Web3Modal instance is", web3Modal);
 }
 
 
@@ -85,7 +72,7 @@ async function fetchAccountData() {
   // Get connected chain id from Ethereum node
   const chainId = await web3.eth.getChainId();
   // Load chain information over an HTTP API
-  const chainData = evmChains.getChain(chainId);
+  const chainData = await EvmChains.getChain(chainId);
   document.querySelector("#network-name").textContent = chainData.name;
 
   // Get list of accounts of the connected wallet
